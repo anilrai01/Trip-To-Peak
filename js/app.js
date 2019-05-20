a2b1c5266a91fc954e40c7335fbce6cc9af5bab6
anil.rai01 <anil.rai01041998@gmail.com>
2019-05-20 20:09:08 +0545

    Personal Travel Agency Site

---
+++ js/app.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $('.scrollspy').scrollSpy();
+    $('.materialboxed').materialbox();
+
+    $('#top-carousel  .owl-carousel').owlCarousel({
+        loop: true,
+        nav: false,
+        pagination: false,
+        autoplay: 2,
+
+        responsive: {
+            0:{
+                items: 1
+            },
+            1000:{
+                items: 1
+            }
+        }
+    });
+
+    $('#package-carousel  .owl-carousel').owlCarousel({
+        loop: true,
+        nav: false,
+        pagination: false,
+        autoplay: 200,
+
+        responsive: {
+            0:{
+                items: 1
+            },
+            1000:{
+                items: 3
+            }
+        }
+    });
+});
+
+$(document).on('click','#home .nav-links li', function() {
+    $(this).addClass('active').siblings().removeClass('active');
+    // console.log('Navvy');
+});
